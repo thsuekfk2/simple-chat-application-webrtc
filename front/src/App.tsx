2d1e36c57fab6ae1a02cdd2096cb36f7c25afa55
thsuekfk2 <thsuekfk2@naver.com>
2023-01-27 01:06:14 +0900

    feat : connect socket

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,8 @@
+import { io } from 'socket.io-client';
+
 function App() {
+  const socket = io(`http://localhost:3000`);
+  console.log(socket);
   return <div className="App">test</div>;
 }
 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
-import http from "http";
-import { Server } from "socket.io";
-import express from "express";
+import http from 'http';
+import { Server } from 'socket.io';
+import express from 'express';
 
 const app = express();
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
+const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST'],
   },
 });
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+}); // 연결확인
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
