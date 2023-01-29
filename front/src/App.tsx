d77eb9105a9b1893ea9ffab74bc0cbc4219d4d67
thsuekfk2 <thsuekfk2@naver.com>
2023-01-30 01:11:15 +0900

    feat : add room joined, lefted notice message

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 
 const socket = io(`http://localhost:3000`);
@@ -8,15 +8,34 @@ function App() {
   const message = useRef<HTMLInputElement>(null);
 
   const [isEnterRoom, setEnterRoom] = useState(false);
+  const [roomName, setRoomName] = useState('');
+  const [chatArray, setChatArray] = useState<String[]>([]);
 
   const submitRoomName = () => {
     socket.emit('join-room', roomNameRef.current?.value);
+
     if (roomNameRef.current) {
+      setRoomName(roomNameRef.current?.value);
       roomNameRef.current.value = '';
     }
 
     setEnterRoom(true);
   };
+
+  useEffect(() => {
+    socket.on('welcome', () => {
+      console.log('someone join in!');
+      setChatArray((prev) => [...prev, 'somone joined!']);
+    });
+    socket.on('bye', () => {
+      console.log('someone lefted !');
+      setChatArray((prev) => [...prev, 'someone lefted!']);
+    });
+    return () => {
+      socket.off('welcome');
+      socket.off('bye');
+    };
+  }, []);
 
   return (
     <div className="App">
@@ -24,6 +43,8 @@ function App() {
         <>
           <input ref={message} />
           <button>Send Message</button>
+          <div>Room Name : {roomName}</div>
+          <div>{chatArray}</div>
         </>
       ) : (
         <>
--- server/server.js
+++ server/server.js
@@ -15,10 +15,19 @@ io.on('connection', (socket) => {
   socket.onAny((event) => {
     console.log('⭐️ Socket Event :', event);
   });
-  console.log(`User Connected: ${socket.id}`); //user socket id
+  console.log(`User Connected , socket ID: ${socket.id}`); //user socket id
   socket.on('join-room', (roomName) => {
     socket.join(roomName); //룸 입장
-    console.log(socket.rooms); //모든 room 정보 조회
+    console.log(socket.rooms); //모든 room id 정보 조회
+
+    //누군가 방에 들어오면 방 안에 있는 모두에게 메세지 보냄
+    socket.to(roomName).emit('welcome', () => {
+      console.log('welcome');
+    });
+  });
+  //클라이언트가 서버와 연결이 끊어지기 전에 마지막 메세지 전송
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => socket.to(room).emit('bye'));
   });
 });
 
