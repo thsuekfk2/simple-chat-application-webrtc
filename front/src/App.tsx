327e9e51dbcd1693db1187353b17cbf389af6765
thsuekfk2 <thsuekfk2@naver.com>
2023-01-31 21:12:06 +0900

    feat : implement socket chat message

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,95 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 const socket = io(`http://localhost:3000`);
 
 function App() {
-  const roomNameRef = useRef<HTMLInputElement>(null);
-  const message = useRef<HTMLInputElement>(null);
+  interface MessageInterface {
+    message: string;
+  }
+
+  const [inputNewMessage, setInputNewMessage] = useState('');
+  const [inputRoomName, setInputRoomName] = useState('');
 
   const [isEnterRoom, setEnterRoom] = useState(false);
   const [roomName, setRoomName] = useState('');
-  const [chatArray, setChatArray] = useState<String[]>([]);
+  const [chatArray, setChatArray] = useState<MessageInterface[]>([]);
 
-  const submitRoomName = () => {
-    socket.emit('join-room', roomNameRef.current?.value);
-
-    if (roomNameRef.current) {
-      setRoomName(roomNameRef.current?.value);
-      roomNameRef.current.value = '';
-    }
-
-    setEnterRoom(true);
+  /** 채팅 onChange 핸들러 */
+  const onMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setInputNewMessage(event.target.value);
   };
 
+  /** 룸 이름 onChange 핸들러 */
+  const onInputRoomNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    event.preventDefault();
+    setInputRoomName(event.target.value);
+  };
+
+  /** 채팅 전송 */
+  const submitMessage = (
+    event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent
+  ) => {
+    event?.preventDefault();
+    socket.emit('chat-message', inputNewMessage, roomName, () => {
+      //마지막 함수는 소켓이 완료되면 호출
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: 'you : ' + inputNewMessage },
+      ]);
+      setInputNewMessage('');
+    });
+  };
+
+  /** 룸 이름 전송 및 저장 */
+  const submitRoomName = () => {
+    socket.emit('join-room', inputRoomName);
+    setRoomName(inputRoomName); //룸 이름 저장
+    setInputRoomName('');
+    setEnterRoom(true); //룸 입장
+  };
+
+  /** 메세지 키보드 입력 함수 */
+  const onKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      if (!event.shiftKey) {
+        event.preventDefault();
+        submitMessage(event);
+      }
+    }
+  };
+
+  /** 채팅 소켓 메세지 */
+  useEffect(() => {
+    socket.on('chat-message', (msg) => {
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: 'other : ' + msg },
+      ]);
+    });
+    return () => {
+      socket.off('chat-message');
+    };
+  }, []);
+
+  /** 룸 입장 및 끊김 소켓 메세지 */
   useEffect(() => {
     socket.on('welcome', () => {
       console.log('someone join in!');
-      setChatArray((prev) => [...prev, 'somone joined!']);
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: 'someone joined!' },
+      ]);
     });
     socket.on('bye', () => {
       console.log('someone lefted !');
-      setChatArray((prev) => [...prev, 'someone lefted!']);
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: 'someone lefted!' },
+      ]);
     });
     return () => {
       socket.off('welcome');
@@ -41,14 +101,23 @@ function App() {
     <div className="App">
       {isEnterRoom ? (
         <>
-          <input ref={message} />
-          <button>Send Message</button>
+          <input
+            value={inputNewMessage || ''}
+            onChange={(e) => onMessageChange(e)}
+            onKeyDown={onKeyPress}
+          />
+          <button onClick={submitMessage}>Send Message</button>
           <div>Room Name : {roomName}</div>
-          <div>{chatArray}</div>
+          {chatArray.map((msg, i) => {
+            return <div key={i}>{msg.message}</div>;
+          })}
         </>
       ) : (
         <>
-          <input ref={roomNameRef} />
+          <input
+            value={inputRoomName}
+            onChange={(e) => onInputRoomNameChange(e)}
+          />
           <button onClick={submitRoomName}>Join Room</button>
         </>
       )}
--- server/server.js
+++ server/server.js
@@ -16,18 +16,25 @@ io.on('connection', (socket) => {
     console.log('⭐️ Socket Event :', event);
   });
   console.log(`User Connected , socket ID: ${socket.id}`); //user socket id
+
+  //join : 지정된 룸에 소켓을 구독하도록 호출
   socket.on('join-room', (roomName) => {
     socket.join(roomName); //룸 입장
     console.log(socket.rooms); //모든 room id 정보 조회
 
     //누군가 방에 들어오면 방 안에 있는 모두에게 메세지 보냄
-    socket.to(roomName).emit('welcome', () => {
-      console.log('welcome');
-    });
+    socket.to(roomName).emit('welcome');
   });
+
   //클라이언트가 서버와 연결이 끊어지기 전에 마지막 메세지 전송
   socket.on('disconnecting', () => {
     socket.rooms.forEach((room) => socket.to(room).emit('bye'));
+  });
+
+  //모든 룸 유저에게 채팅 메세지 보내기
+  socket.on('chat-message', (message, roomName, done) => {
+    socket.to(roomName).emit('chat-message', message);
+    done();
   });
 });
 
