774818b529f75eddf0cd970c1e1dc314dade9efc
thsuekfk2 <thsuekfk2@naver.com>
2023-02-12 20:59:41 +0900

    refactor : refactoring home page with atomic

--- front/src/components/atoms/BounceIcon.tsx
+++ front/src/components/atoms/BounceIcon.tsx
@@ -5,8 +5,5 @@ interface Props {
 }
 
 export const BounceIcon = ({ icon }: Props) => {
-  return (
-    // absolute left-[97%] top-1/2 w-[50px] animate-bounce text-5xl
-    <div className="absolute  animate-bounce ">{icon}</div>
-  );
+  return <div className="absolute  animate-bounce ">{icon}</div>;
 };
--- front/src/components/pages/Home.tsx
+++ front/src/components/pages/Home.tsx
@@ -1,180 +1,41 @@
-import { useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
 import { BounceIcon } from '../atoms/BounceIcon';
-import { Button } from '../atoms/Button';
-import { InputButton } from '../molecules/InputButton';
+import { EnterForm } from '../organisms/EnterForm';
 
-const socket = io(`http://localhost:3000`);
-
-type RoomList = {
-  roomName: String;
-  roomCount: Number;
-};
 export const Home = () => {
-  interface MessageInterface {
-    message: string;
-  }
-
-  const [inputNewMessage, setInputNewMessage] = useState('');
-  const [inputRoomName, setInputRoomName] = useState('');
-  const [inputNickname, setNickname] = useState('');
-
-  const [isEnterRoom, setEnterRoom] = useState(false);
-  const [roomName, setRoomName] = useState('');
-  const [chatArray, setChatArray] = useState<MessageInterface[]>([]);
-
-  const [publicRoomList, setPublicRoomList] = useState<RoomList[]>([]);
-
-  /** 닉네임 onChange 핸들러 */
-  const onNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setNickname(event.target.value);
-  };
-
-  /** 채팅 onChange 핸들러 */
-  const onMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setInputNewMessage(event.target.value);
-  };
-
-  /** 룸 이름 onChange 핸들러 */
-  const onInputRoomNameChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    event.preventDefault();
-    setInputRoomName(event.target.value);
-  };
-
-  /** 채팅 전송 */
-  const submitMessage = (
-    event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent
-  ) => {
-    event?.preventDefault();
-    socket.emit('chat-message', inputNewMessage, roomName, () => {
-      //마지막 함수는 소켓이 완료되면 호출
-      setChatArray((prev: MessageInterface[]) => [
-        ...prev,
-        { message: `${inputNickname} : ` + inputNewMessage },
-      ]);
-      setInputNewMessage('');
-    });
-  };
-
-  /** 룸 이름 전송 및 저장 */
-  const submitRoomName = () => {
-    //닉네임 미지정 시 초기 닉네임 설정
-    if (inputNickname === '') {
-      setNickname('me');
-    }
-    socket.emit('join-room', inputRoomName);
-    setRoomName(inputRoomName); //룸 이름 저장
-    setInputRoomName('');
-    setEnterRoom(true); //룸 입장
-  };
-
-  /** 닉네임 전송 및 저장 */
-  const submitNickname = () => {
-    socket.emit('nick-name', inputNickname);
-  };
-
-  /** 메세지 키보드 입력 함수 */
-  const onKeyPress = (event: React.KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      if (!event.shiftKey) {
-        event.preventDefault();
-        submitMessage(event);
-      }
-    }
-  };
-
-  /** 전체 public 룸 조회 소켓 메세지 */
-  useEffect(() => {
-    socket.on('room-change', (publicRoom) => {
-      console.log(publicRoom);
-      setPublicRoomList(publicRoom);
-    });
-    return () => {
-      socket.off('room-change');
-    };
-  }, []);
-
-  /** 채팅 소켓 메세지 */
-  useEffect(() => {
-    socket.on('chat-message', (msg, user) => {
-      setChatArray((prev: MessageInterface[]) => [
-        ...prev,
-        { message: `${user} : ` + msg },
-      ]);
-    });
-    return () => {
-      socket.off('chat-message');
-    };
-  }, []);
-
-  /** 룸 입장 및 끊김 소켓 메세지 */
-  useEffect(() => {
-    socket.on('welcome', (user) => {
-      console.log('someone join in!');
-      setChatArray((prev: MessageInterface[]) => [
-        ...prev,
-        { message: `${user} joined!` },
-      ]);
-    });
-    socket.on('bye', (user) => {
-      console.log('someone lefted !');
-      setChatArray((prev: MessageInterface[]) => [
-        ...prev,
-        { message: `${user} lefted!` },
-      ]);
-    });
-    return () => {
-      socket.off('welcome');
-      socket.off('bye');
-    };
-  }, []);
-
+  const publicRoomListDummy = [
+    { roomName: 'dd', roomCount: 1 },
+    { roomName: 'dd', roomCount: 1 },
+    { roomName: 'dd', roomCount: 1 },
+    { roomName: 'dd', roomCount: 1 },
+    { roomName: 'dd', roomCount: 1 },
+    { roomName: 'dd', roomCount: 1 },
+    { roomName: 'dd', roomCount: 1 },
+    { roomName: 'dd', roomCount: 1 },
+  ];
   return (
-    <div className="h-screen w-screen ">
-      <div className="bg-subColor fix absolute h-full w-4/5 min-w-[330px] transition-all md:w-3/5 lg:w-2/5">
-        <div className="absolute left-[97%] top-[48%] text-2xl sm:text-5xl">
-          <BounceIcon icon={'☺'} />
-        </div>
-        {isEnterRoom ? (
-          <>
-            <input
-              value={inputNewMessage || ''}
-              onChange={(e) => onMessageChange(e)}
-              onKeyDown={onKeyPress}
-            />
-            <Button label="Send Message" onClick={() => submitMessage} />
-            <div>Room Name : {roomName}</div>
-            {chatArray.map((msg, i) => {
-              return <div key={i}>{msg.message}</div>;
-            })}
-          </>
-        ) : (
-          <div className=" absolute flex h-full w-full flex-col items-center justify-center gap-4">
-            <div className="text-xl">Welcome 👋</div>
-            <InputButton
-              placeholder="your name"
-              value={inputNickname || ''}
-              onChange={(e: any) => onNicknameChange(e)}
-              submit={submitNickname}
-              label={'Save'}
-            />
-            <InputButton
-              placeholder="Room name"
-              value={inputRoomName}
-              onChange={(e) => onInputRoomNameChange(e)}
-              submit={submitRoomName}
-              label={'Enter'}
-            />
-          </div>
-        )}
-        {!isEnterRoom &&
-          publicRoomList.map((room) => {
-            return <div>{`${room?.roomName}(${room?.roomCount})`}</div>;
+    <div className="flex h-screen w-screen flex-row overflow-hidden">
+      <div className="bg-subColor flex h-full w-2/5 min-w-[335px] justify-center">
+        <EnterForm />
+      </div>
+      <div className=" relative right-4 flex items-center text-2xl sm:text-5xl">
+        <BounceIcon icon={'☺'} />
+      </div>
+
+      <div className="flex flex-col bg-gray-light h-full w-3/5 min-w-[200px] items-center justify-center gap-4">
+        <div className=" font-bold text-lg"> 열려있는 채팅방들 👀 </div>
+        <div className="flex flex-wrap overflow-y-auto  justify-center items-center content-center gap-3">
+          {publicRoomListDummy.length === 0 && (
+            <div>열려있는게 없네요 ㅜ.ㅜ</div>
+          )}
+          {publicRoomListDummy?.map((room, i) => {
+            return (
+              <div
+                key={i}
+                className="flex w-[200px] h-[100px] bg-mainColor rounded"
+              >{`${room?.roomName}(${room?.roomCount})`}</div>
+            );
           })}
+        </div>
       </div>
     </div>
   );
