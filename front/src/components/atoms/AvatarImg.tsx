a63a7ea782377aed54439fc14fdd4e3902633389
thsuekfk2 <thsuekfk2@naver.com>
2023-02-13 23:45:22 +0900

    feat : install random avatar, add avatar component

---
+++ front/src/components/atoms/AvatarImg.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useMemo, useState } from 'react';
+import { createAvatar } from '@dicebear/core';
+import { thumbs } from '@dicebear/collection';
+
+interface Props {
+  roomId?: string;
+}
+
+function Avatar({ roomId }: Props) {
+  const avatar = useMemo(() => {
+    return createAvatar(thumbs, {
+      seed: roomId,
+      backgroundColor: ['b6e3f4', 'c0aede', 'd1d4f9'],
+      eyes: [
+        'variant1W10',
+        'variant2W12',
+        'variant3W14',
+        'variant4W16',
+        'variant6W10',
+        'variant7W12',
+        'variant8W14',
+        'variant9W16',
+      ],
+      eyesColor: ['000000', 'ffffff'],
+      face: ['variant1', 'variant2', 'variant3', 'variant4', 'variant5'],
+      faceOffsetX: [-15, 15],
+      faceOffsetY: [-15, 15],
+      mouth: ['variant1', 'variant2', 'variant3', 'variant4', 'variant5'],
+      shapeColor: ['0a5b83', '1c799f', '69d2e7', 'f1f4dc', 'f88c49'],
+      randomizeIds: true,
+    }).toDataUriSync();
+  }, []);
+
+  return <img src={avatar} alt="" className="w-[100px] rounded-full" />;
+}
+
+export default Avatar;
