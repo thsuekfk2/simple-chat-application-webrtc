745c2cb934a55a6bb67d8bfc1b523dea0f0473d9
thsuekfk2 <thsuekfk2@naver.com>
2023-02-16 01:01:32 +0900

    feat : setting room layout

--- front/src/components/pages/Room.tsx
+++ front/src/components/pages/Room.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Avatar from '../atoms/AvatarImg';
+import { roomSocket } from '../../adapters/roomSocket';
+import { InputButton } from '../molecules/InputButton';
 
 type RoomList = {
   roomName: String;
@@ -21,80 +24,104 @@ export const Room = () => {
     setInputNewMessage(event.target.value);
   };
 
-  // useEffect(() => {
-  //   generateSocket();
-  // }, []);
-
   /** 채팅 전송 */
-  // const submitMessage = (
-  //   event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent
-  // ) => {
-  //   event?.preventDefault();
-  //   roomSockets.emit('chat-message', inputNewMessage, roomName, () => {
-  //     //마지막 함수는 소켓이 완료되면 호출
-  //     setChatArray((prev: MessageInterface[]) => [
-  //       ...prev,
-  //       { message: `${inputNickname} : ` + inputNewMessage },
-  //     ]);
-  //     setInputNewMessage('');
-  //   });
-  // };
+  const submitMessage = (
+    event: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent
+  ) => {
+    event?.preventDefault();
+    roomSocket?.emit('chat-message', inputNewMessage, 'test', () => {
+      //마지막 함수는 소켓이 완료되면 호출
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: `${'Dd'} : ` + inputNewMessage },
+      ]);
+      setInputNewMessage('');
+    });
+  };
 
   /** 메세지 키보드 입력 함수 */
-  // const onKeyPress = (event: React.KeyboardEvent) => {
-  //   if (event.key === 'Enter') {
-  //     if (!event.shiftKey) {
-  //       event.preventDefault();
-  //       submitMessage(event);
-  //     }
-  //   }
-  // };
-
-  /** 전체 public 룸 조회 소켓 메세지 */
-  // useEffect(() => {
-  //   roomSockets.on('room-change', (publicRoom: string) => {
-  //     console.log(publicRoom);
-  //     setPublicRoomList(publicRoom);
-  //   });
-  //   return () => {
-  //     roomSockets.off('room-change');
-  //   };
-  // }, []);
+  const onKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      if (!event.shiftKey) {
+        event.preventDefault();
+        submitMessage(event);
+      }
+    }
+  };
 
   /** 채팅 소켓 메세지 */
-  // useEffect(() => {
-  //   roomSockets.on('chat-message', (msg: string, user: string) => {
-  //     setChatArray((prev: MessageInterface[]) => [
-  //       ...prev,
-  //       { message: `${user} : ` + msg },
-  //     ]);
-  //   });
-  //   return () => {
-  //     roomSockets.off('chat-message');
-  //   };
-  // }, []);
+  useEffect(() => {
+    roomSocket?.on('chat-message', (msg: string, user: string) => {
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: `${user} : ` + msg, type: '' },
+      ]);
+    });
+    return () => {
+      roomSocket?.off('chat-message');
+    };
+  }, []);
 
   /** 룸 입장 및 끊김 소켓 메세지 */
-  // useEffect(() => {
-  //   roomSockets.on('welcome', (user: string) => {
-  //     console.log('someone join in!');
-  //     setChatArray((prev: MessageInterface[]) => [
-  //       ...prev,
-  //       { message: `${user} joined!` },
-  //     ]);
-  //   });
-  //   roomSockets.on('bye', (user: string) => {
-  //     console.log('someone lefted !');
-  //     setChatArray((prev: MessageInterface[]) => [
-  //       ...prev,
-  //       { message: `${user} lefted!` },
-  //     ]);
-  //   });
-  //   return () => {
-  //     roomSockets.off('welcome');
-  //     roomSockets.off('bye');
-  //   };
-  // }, []);
+  useEffect(() => {
+    roomSocket?.on('welcome', (user: string) => {
+      console.log('someone join in!');
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: `${user} joined!`, type: 'notice' },
+      ]);
+    });
+    roomSocket?.on('bye', (user: string) => {
+      console.log('someone lefted !');
+      setChatArray((prev: MessageInterface[]) => [
+        ...prev,
+        { message: `${user} lefted!`, type: 'notice' },
+      ]);
+    });
+    return () => {
+      roomSocket?.off('welcome');
+      roomSocket?.off('bye');
+    };
+  }, []);
 
-  return <div>채팅 룸 페이지</div>;
+  return (
+    <div className=" bg-mainColor w-full h-screen overflow-y-auto">
+      <div className="flex justify-start items-center m-2">
+        <Avatar roomId={'test'} />
+        <div className="pl-4">룸 타이틀</div>
+      </div>
+      <div className="flex flex-wrap mt-5">
+        <div className="flex-1 bg-gray-dark w-full md:min-w-[300px] min-h-[500px]">
+          스크린
+        </div>
+        <div className="text-xs flex flex-col flex-2 bg-subColor w-full md:w-[300px] min-w-[300px] h-[500px] align-middle items-center justify-between">
+          {/* 채팅창 */}
+          <div>
+            {chatArray.map((data: any, i) => {
+              if (data.type === 'notice')
+                return (
+                  <div
+                    className="w-[100%] h-[40px] bg-blue rounded flex items-center justify-center m-3 p-2 "
+                    key={i}
+                  >
+                    {data.message}
+                  </div>
+                );
+            })}
+          </div>
+
+          {/* 채팅 입력 */}
+          <div className="w-full">
+            <InputButton
+              value={inputNewMessage || ''}
+              onChange={(e: any) => onMessageChange(e)}
+              onKeyDown={onKeyPress}
+              label={'Save'}
+              submit={submitMessage}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
