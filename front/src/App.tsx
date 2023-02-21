1a65081f8fe4cf9a06e455a0e33d7515dd1a1bac
thsuekfk2 <thsuekfk2@naver.com>
2023-02-21 23:34:39 +0900

    chore : npm recoil-persist, recoil setting

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './components/pages/Home';
 import { Room } from './components/pages/Room';
 
+import { RecoilRoot } from 'recoil';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/room" element={<Room />} />
-      </Routes>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room" element={<Room />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 }
 
